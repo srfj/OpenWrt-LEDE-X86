2c88aa965a18f7eb692b7a8c86456158463a25b4
srfj <122041136+srfj@users.noreply.github.com>
2024-11-02 14:55:52 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Themes
+# 移除要替换的包
+rm -rf feeds/luci/themes/luci-theme-argon
+# 添加主题
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+
+# 添加软件包
+git clone https://github.com/srfj/luci-app-mwan3helper-chinaroute package/luci-app-mwan3helper-chinaroute
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
